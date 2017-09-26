3e259ab446a0d22f24209a428c191f944e79f6f4
wlzx120 <wlzx120@163.com>
2017-09-26 10:07:26 +0800

    finish initial-configs

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,42 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    // 配置语言
+    'language'=>'zh-CN',
+    // 配置时区
+    'timeZone'=>'Asia/Chongqing',
     'components' => [
+        // 配置缓存
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        // 配置数据库
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 24*3600,
+            'schemaCache' => 'cache',
+        ],
+        //url美化
+        "urlManager" => [
+            //用于表明urlManager是否启用URL美化功能，在Yii1.1中称为path格式URL，
+            "enablePrettyUrl" => true,
+            // 是否启用严格解析，如启用严格解析，要求当前请求应至少匹配1个路由规则，
+            // 否则认为是无效路由。
+            // 这个选项仅在 enablePrettyUrl 启用后才有效。
+            "enableStrictParsing" => false,
+            // 是否在URL中显示入口脚本。是对美化功能的进一步补充。
+            "showScriptName" => false,
+            // 指定续接在URL后面的一个后缀，如 .html 之类的。仅在 enablePrettyUrl 启用时有效。
+            "suffix" => "",
+            "rules" => [
+                "<controller:\w+>/<id:\d+>"=>"<controller>/view",
+                "<controller:\w+>/<action:\w+>"=>"<controller>/<action>"
+            ],
         ],
     ],
 ];
